6160e7f56399c247dcf006aabe09cd131b49b01a
Lannister1234 <luxingj@andrew.cmu.edu>
2018-04-28 16:57:15 -0400

    add accuracy of each class

--- read_features.py
+++ read_features.py
@@ -1,10 +1,10 @@
 import numpy as np
 from sklearn.svm import SVC
+from collections import Counter
 
 import pdb
 
-
-f = open("feature_and_labels.txt", 'r') 
+f = open("feature_and_labels1.txt", 'r')
 
 labels = []
 features = []
@@ -17,6 +17,8 @@ for line in f.readlines():
     features.append(temp)
 f.close()
 
+print(Counter(labels))
+
 #create test and train samples indice
 all_samples_num = len(labels)
 temp = np.arange(all_samples_num)
@@ -25,20 +27,21 @@ np.random.shuffle(temp)
 train_sample_indice = temp[:int(len(temp) *0.8)]
 test_sample_indice = temp[int(len(temp) *0.8):]
 
-
 X_samples = np.asarray(features)    # n_samples * n_features
 y_samples = np.asarray(labels)   # n_samples * 1
 
-pdb.set_trace()
-X_train = X_samples[train_sample_indice,:]
+
+# pdb.set_trace()
+X_train = X_samples[train_sample_indice, :]
 y_train = []
 for i in train_sample_indice:
 	y_train.append(y_samples[i])
 y_train = np.asarray(y_train)
+print(Counter(y_train))
 
 # can change the multiclass classifier
 print("training ..")
-print("sample nums",len(y_train))
+print("sample nums", len(y_train))
 clf = SVC()
 clf.fit(X_train, y_train)
 
@@ -54,17 +57,43 @@ y_true = []
 for i in test_sample_indice:
 	y_true.append(y_samples[i])
 y_true = np.asarray(y_true)
+print(Counter(y_true))
 
 print("classifying ...")
 y_pred = clf.predict(X_pred)
-# print(y_pred)
-# print(y_train)
-
+print(y_pred)
+print(y_train)
 # pdb.set_trace()
 
-acc = 0.0
-for i in range(len(y_pred)):
-    if(y_pred[i]==y_true[i]):
-        acc += 1
-print("acc:",acc/len(y_pred))
+label_to_index = {
+	"Peaceful": 0,
+    "Passion": 1,
+    "Fear": 2,
+    "Happiness": 3,
+	"Sadness": 4,
+}
 
+acc = 0.0
+correct_num_class = [0 for x in range(5)]
+sum_class = [0 for x in range(5)]
+misclassfied_files = {}
+for i in range(len(y_pred)):
+    predict_class = label_to_index[y_pred[i]]
+    true_class = label_to_index[y_true[i]]
+    sum_class[true_class] += 1
+
+    if(y_pred[i]==y_true[i]):
+        correct_num_class[predict_class] += 1
+        acc += 1
+    else:
+        fileList = misclassfied_files.get(y_true[i], list())
+        fileList.append(filenames[i])
+        misclassfied_files[y_true[i]] = fileList
+
+print("correct in each class: ")
+print(correct_num_class)
+print("total number in each class")
+print(sum_class)
+print("total accuracy:", acc/len(y_pred))
+print("misclassied files")
+print(misclassfied_files)
